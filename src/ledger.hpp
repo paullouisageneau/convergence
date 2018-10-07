d10a3bbbad93f1050cfdb8a7a67f0db80fb49823
Paul-Louis Ageneau <paul-louis@ageneau.org>
2018-10-07 18:03:14 +0200

    Added ledger

---
+++ src/ledger.hpp
@@ -0,0 +1,53 @@
+/***************************************************************************
+ *   Copyright (C) 2017-2018 by Paul-Louis Ageneau                         *
+ *   paul-louis (at) ageneau (dot) org                                     *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.           *
+ ***************************************************************************/
+
+#ifndef CONVERGENCE_LEDGER_H
+#define CONVERGENCE_LEDGER_H
+
+#include "src/include.hpp"
+
+namespace convergence
+{
+
+class Ledger
+{
+public:
+	class Entry
+	{
+		Entry(void);
+		Entry(const binary &data);
+		
+		binary toBinary(void);
+	};
+
+	Ledger(void);
+	~Ledger(void);
+
+	void append(const Entry &entry);
+
+private:
+	std::map<binary, Entry> mEntries;
+	binary mCurrent;
+};
+
+}
+
+#endif
+
