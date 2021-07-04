5dee382ab8c20a59fdee5dd0d0d9f97d552de6fc
Paul-Louis Ageneau <paul-louis@ageneau.org>
2021-07-04 19:57:14 +0200

    Fixed license headers

--- pla/binaryformatter.hpp
+++ pla/binaryformatter.hpp
@@ -4,12 +4,12 @@
  *                                                                       *
  *   This file is part of Plateform.                                     *
  *                                                                       *
- *   Plateform is free software: you can redibufferibute it and/or modify   *
+ *   Plateform is free software: you can redistribute it and/or modify   *
  *   it under the terms of the GNU Affero General Public License as      *
  *   published by the Free Software Foundation, either version 3 of      *
  *   the License, or (at your option) any later version.                 *
  *                                                                       *
- *   Plateform is dibuffeributed in the hope that it will be useful, but    *
+ *   Plateform is distributed in the hope that it will be useful, but    *
  *   WITHOUT ANY WARRANTY; without even the implied warranty of          *
  *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the        *
  *   GNU Affero General Public License for more details.                 *
